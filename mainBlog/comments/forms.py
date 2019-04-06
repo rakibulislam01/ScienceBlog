8bb7b46328906fa2b95e194cc7ee968f697527c3
rakibulislam01 <mrakibul70@gmail.com>
2019-04-06 13:51:42 +0600

    Add comment reply

--- mainBlog/comments/forms.py
+++ mainBlog/comments/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
-    parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    # parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
     content = forms.CharField(widget=forms.Textarea)
 
 
--- mainBlog/comments/models.py
+++ mainBlog/comments/models.py
@@ -4,12 +4,15 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
-# Create your models here.
 class ContentManager(models.Manager):
+    def all(self):
+        qs = super(ContentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(ContentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(ContentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 
@@ -19,11 +22,23 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = ContentManager()
 
+    class Meta:
+        ordering = ['-timestamp']
+
     def __str__(self):
         return str(self.user.username)
+
+    def children(self):  # replies
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
