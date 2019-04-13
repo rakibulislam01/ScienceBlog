755e60c104ae681282590724e987ccea5164bbe0
rakibulislam01 <mrakibul70@gmail.com>
2019-04-13 17:56:00 +0600

    Add comment delete part2 delete permission and Http status code.

--- mainBlog/comments/models.py
+++ mainBlog/comments/models.py
@@ -37,6 +37,9 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return reverse("thread", kwargs={'id': self.id})
 
+    def get_delete_url(self):
+        return reverse("delete", kwargs={'id': self.id})
+
     def children(self):  # replies
         return Comment.objects.filter(parent=self)
 
